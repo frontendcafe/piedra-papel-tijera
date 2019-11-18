1ea2c14fdac4f2f2db5defdcbd7d0b59a40bd893
Agus Mar <agustinam@gmail.com>
2019-11-18 18:16:07 -0300

    update react app

---
+++ client/src/components/Rules/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Rules extends React.Component {
+  render() {
+    return (
+      <div>
+        Rules
+      </div>
+    );
+  }
+}
+
+export default Rules;
---
+++ client/src/components/Score/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Score extends React.Component {
+  render() {
+    return (
+      <div>
+        Score
+      </div>
+    );
+  }
+}
+
+export default Score;
