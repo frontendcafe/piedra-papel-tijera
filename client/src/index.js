7d2525b0f3f60312deeb04f17be3e1c968afe8d7
Elvis <elvisgiltorres@gmail.com>
2022-04-05 16:08:49 -0400

    Actualizando el archivo index.js a una version mas moderna de React(V:18.0)

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root')); OLD!!!!
+
+//Nuevo metodo implementado en la Version 18.0 de React
+
+    const root = document.getElementById('root');
+    const container = ReactDOMClient.createRoot(root);
+    container.render(<App />)
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
